c578474ecd6bd6ac8e70eb7a0de3486f896be711
Hunter Strathman <hstrathman@gmail.com>
2020-10-07 11:17:16 -0600

    Updating before moving data to cloud. Shouldn't be any huge changes here, but looks like there are a coupole of new scripts and a modification to _RippleAPAll. It's been so long, idk what this is though.

--- analysis_matlab/plotScript_20191122_PracticeThesisProposal.m
+++ analysis_matlab/plotScript_20191122_PracticeThesisProposal.m
@@ -1,5 +1,5 @@
-%% plotScript: Ripple Action potentials
-% Quick script to plot threshold crossings from Ripple recordings
+%% plotScript: For Thesis Proposal
+% Adapted from plotScript_RippleAPAll
 
 close all 
 clearvars 
@@ -46,38 +46,31 @@ for ii = 1:numChannelsRip
     dataStructure(ii).threshColor = [.5 .5 .5];
 end
 
-%% Spike Sorting
-% First detect, then grab threshold crossing events.
-rejectMod =  1.7;
-ARP       = .001;
-threshold = -6.5;
-[~, totalChannels] = size(dataStructure);
-for ii = 1:totalChannels
-    [spikesIndex, ~] = ...
-        spike_detection(dataStructure(ii).filteredData,threshold,1,0);
-    [dataStructure(ii).waveforms, dataStructure(ii).timeWave, spikesIndex] = ...
-        waveformGrabber(dataStructure(ii).filteredData, ...
-        spikesIndex, 1.6, dataStructure(ii).Fs); % Must be more than two spike events
-    
-%removes "bad" spikes
-    [dataStructure(ii).waveformSorted, spikeEventsNew] = ...
-        templateMatcher(dataStructure(ii).waveforms, ...
-                        rejectMod,...
-                        spikesIndex, ...
-                        ARP, ...
-                        dataStructure(ii).Fs); 
-    dataStructure(ii).meanWave = mean(dataStructure(ii).waveformSorted);
-    dataStructure(ii).spikeTimes = dataStructure(ii).time(spikeEventsNew);
-end
+%% Plots
+figure(1)
+plot(dataStructure(2).time,dataStructure(2).rawData)
+xlim([9.1 12])
+ylim([-300 300])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
 
-for ii = 1:totalChannels
-    figure(dataStructure(ii).electrode)
-    plot(dataStructure(ii).timeWave*1e3, dataStructure(ii).waveformSorted, ...
-        'Color', dataStructure(ii).threshColor, ...
-        'LineWidth', 1.2)
-    hold on
-    plot(dataStructure(ii).timeWave*1e3, dataStructure(ii).meanWave, 'LineWidth', 3.5)
-    ylim([-30 30])
-    xlabel('Time (ms)')
-    ylabel('Amplitude (uV)')
-end
+figure(2)
+plot(dataStructure(2).time,dataStructure(2).rawData)
+xlim([10.3 10.8])
+ylim([-300 100])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
+
+figure(3)
+plot(dataStructure(2).time,dataStructure(2).rawData)
+xlim([10.510 10.543])
+ylim([-300 100])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
+
+figure(4)
+plot(dataStructure(2).time,dataStructure(2).rawData, 'LineWidth', 1.5)
+xlim([10.524 10.53])
+ylim([-130 -40])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
---
+++ analysis_matlab/plotScript_20191122_PracticeThesisProposal_TBCASPres_edit.m
@@ -0,0 +1,83 @@
+%% plotScript: For Thesis Proposal
+% Adapted from plotScript_RippleAPAll
+
+close all 
+clearvars 
+
+% Sets relative filepaths from this script
+currentFile = mfilename( 'fullpath' );
+cd(fileparts(currentFile));
+addpath(genpath('../matlab'));
+addpath(genpath('../rawData'));
+addpath(genpath('../output'));
+parts = strsplit(currentFile, {'\', '\'});
+outputDir = ['../output/' parts{end}];
+[~, ~] = mkdir(outputDir);
+
+%%
+rippleFileName = 'SD190719A_SurgeryInsertion_20190719_1.ns5';
+hpCornerFreq   =  300;
+lpCornerFreq   = 7500;
+
+% Ripple Data. Ripple uses .25 uV per bit. Data comes in as bits. Here
+% we're leaving units as uV [mini function candidate]
+[ NSxFileArray, NSxbasicHeader, NSxchannelHeader, NSxTimeStamps ] = ...
+    NSxGetMemMapFile( rippleFileName );
+rawdata = NSxFileArray.Data.NSxData;
+V = double( rawdata );
+timeRipple = double( NSxTimeStamps ) / 30000;
+VRipple = ( V  )/4;
+
+%% Filter Data
+% Data from both instruments is stored in a single data structure to allow
+% a single for loop to take care of all the spike sorting below. 
+% Ripple. 
+[ dataFiltRipple ] = filterFunc(VRipple, 30e3, 3, ...
+               'highPassCorner', hpCornerFreq, ...
+               'lowPassCorner', lpCornerFreq);
+[numChannelsRip, ~] = size(dataFiltRipple);
+for ii = 1:numChannelsRip
+    dataStructure(ii).rawData = VRipple(ii,:);
+    dataStructure(ii).filteredData = dataFiltRipple(ii,:);
+    dataStructure(ii).Fs = 30e3;
+    dataStructure(ii).instrument = 'Ripple';
+    dataStructure(ii).electrode = ii;
+    dataStructure(ii).time = timeRipple;
+    dataStructure(ii).threshColor = [.5 .5 .5];
+end
+
+%% Plots
+figure(1)
+plot(dataStructure(2).time,dataStructure(2).rawData)
+xlim([9.1 12])
+ylim([-300 300])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
+
+figure(2)
+plot(dataStructure(2).time,dataStructure(2).rawData)
+xlim([10.3 10.8])
+ylim([-300 100])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
+
+figure(3)
+plot(dataStructure(2).time,dataStructure(2).rawData)
+xlim([10.520 10.534])
+ylim([-200 0])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
+
+figure(6)
+plot(dataStructure(2).time,dataStructure(2).filteredData)
+xlim([10.515 10.55])
+% ylim([-200 0])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
+
+figure(4)
+plot(dataStructure(2).time,dataStructure(2).rawData, 'LineWidth', 1.5)
+xlim([10.524 10.53])
+ylim([-130 -40])
+ylabel('Voltage (uV)')
+xlabel('Time (s)')
